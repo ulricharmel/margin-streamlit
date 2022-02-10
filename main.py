ef3f19821de586a442aa850114bbbc70069fa06f
ulricharmel <mulricharmel@gmail.com>
2022-02-10 22:54:46 +0200

    add __main__

--- main.py
+++ main.py
@@ -28,30 +28,31 @@ uploaded_file = st.file_uploader("Upload Image")
 
 # text over upload button "Upload Image"
 
-if uploaded_file is not None:
+if __name__ == "__main__":
+    if uploaded_file is not None:
 
-    if save_uploaded_file(uploaded_file): 
+        if save_uploaded_file(uploaded_file): 
 
-        # display the image
+            # display the image
 
-        display_image = Image.open(uploaded_file)
+            display_image = Image.open(uploaded_file)
 
-        st.image(display_image)
+            st.image(display_image)
 
-        prediction, prob = predictor(os.path.join('static/images',uploaded_file.name))
+            prediction, prob = predictor(os.path.join('static/images',uploaded_file.name))
 
-        os.remove('static/images/'+uploaded_file.name)
+            os.remove('static/images/'+uploaded_file.name)
 
-        # deleting uploaded saved picture after prediction
+            # deleting uploaded saved picture after prediction
 
-        # drawing graphs
+            # drawing graphs
 
-        st.text(f'Model predicts {prediction} with a {prob}% confidence!')
+            st.text(f'Model predicts {prediction} with a {prob}% confidence!')
 
-        # fig, ax = plt.subplots()
+            # fig, ax = plt.subplots()
 
-        # ax  = sns.barplot(y = 'name',x='values', data = prediction,order = prediction.sort_values('values',ascending=False).name)
+            # ax  = sns.barplot(y = 'name',x='values', data = prediction,order = prediction.sort_values('values',ascending=False).name)
 
-        # ax.set(xlabel='Confidence %', ylabel='Breed')
+            # ax.set(xlabel='Confidence %', ylabel='Breed')
 
-        # st.pyplot(fig)
+            # st.pyplot(fig)
